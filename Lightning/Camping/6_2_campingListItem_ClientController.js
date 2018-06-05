fc562960696638a3c0eb171610e2a04a6499f506
Sudeep <sudeep.ghag@gmail.com>
2018-06-05 01:03:25 -0700

    Update 6_2_campingListItem_ClientController.js

--- Lightning/Camping/6_2_campingListItem_ClientController.js
+++ Lightning/Camping/6_2_campingListItem_ClientController.js
@@ -2,7 +2,13 @@
     packItem: function(component, event, helper) {
         var btnClicked = event.getSource();         // the button
         btnClicked.set("v.disabled", true); 
-        component.set("v.item.Packed__c", "true");     
+        component.set("v.item.Packed__c", "false");    
+
+        var updatedItem = component.get("v.item");  
+        var updateItemEvent = component.getEvent("updateItem");
+        updateItemEvent.setParams({ "item": updatedItem });
+        updateItemEvent.fire();   
+    
     },    
     
     getItems: function(cmp){
