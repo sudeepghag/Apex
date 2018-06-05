4848822c0d3dcbe721c1214236a8c3ac34bc94ee
Sudeep <sudeep.ghag@gmail.com>
2018-06-05 01:07:40 -0700

    Update 6_2_campingListItem_ClientController.js

--- Lightning/Camping/6_2_campingListItem_ClientController.js
+++ Lightning/Camping/6_2_campingListItem_ClientController.js
@@ -9,36 +9,5 @@
         updateItemEvent.setParams({ "item": updatedItem });
         updateItemEvent.fire();   
     
-    },    
-    
-    getItems: function(cmp){
-        var action = cmp.get("c.getCampingItems");
-        action.setCallback(this, function(response){
-            var state = response.getState();
-            if (state === "SUCCESS") {
-                cmp.set("v.items", response.getReturnValue());
-            }
-        });
-	 $A.enqueueAction(action);
-    },
- 
-    loadFirstItem: function(cmp){
-        var action = cmp.get("c.getFirstCampingItem");
-        action.setCallback(this, function(response){
-            var state = response.getState();
-            if (state === "SUCCESS") {
-                cmp.set("v.item", response.getReturnValue());
-            }
-        });
-	 $A.enqueueAction(action);
-    },
-
-    SaveItem: function(component, event, helper) {
-        var btnClicked = event.getSource();         // the button
-        var btnMessage = btnClicked.get("v.label"); // the button's label
-        component.set("v.message", btnMessage);     // update our message
     }
-
-
-    
 })
