a02f1486a1a10c733ee852833eaf1eced4e43f71
Sudeep <sudeep.ghag@gmail.com>
2018-06-07 16:59:29 -0700

    Update 3_ContactListSearch_ClientController.js

--- Lightning/In-Context Search/3_ContactListSearch_ClientController.js
+++ Lightning/In-Context Search/3_ContactListSearch_ClientController.js
@@ -8,28 +8,34 @@
         ]);
         
         helper.fetchContacts(component);
+        component.set("v.searchApplied","false");
 
 	},
     searchKeyChange: function(component, event, helper) {
      	var searchKey = component.get("v.searchKey"); //event.target.value;
         var accountId = component.get("v.recordId");
+        
+        if(searchKey.length > 0){
+            alert('Client-Controller-searchKeyChange / searchKey: ' + searchKey + ' / accountId: '+ accountId);
             
-        alert('Client-Controller-searchKeyChange / searchKey: ' + searchKey + ' / accountId: '+ accountId);
-        
-        var action = component.get("c.filterByKey");
-        action.setParams({
-          "searchKey": searchKey,
-          "pAccountId" : accountId
-        }); 
-        action.setCallback(this, function(a) {
-            component.set("v.Contacts", a.getReturnValue());
-        });
-        
-        $A.enqueueAction(action);  		
+            var action = component.get("c.filterByKey");
+            action.setParams({
+              "searchKey": searchKey,
+              "pAccountId" : accountId
+            }); 
+            action.setCallback(this, function(a) {
+                component.set("v.Contacts", a.getReturnValue());
+                component.set("v.count", a.getReturnValue().length);    
+                component.set("v.searchApplied","true");
+            });
+            
+            $A.enqueueAction(action);  
+		}
         
  },
     clearSearch:function(component, event, helper) {
         component.set("v.searchKey","");
+        component.set("v.searchApplied","false");
         helper.fetchContacts(component);
     }
     
