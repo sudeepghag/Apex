31d20cc1adb41b0565c7db830d3f65c0542dae94
Sudeep <sudeep.ghag@gmail.com>
2018-06-07 15:52:15 -0700

    Create 4_ContactListSearch_Helper.js

---
+++ Lightning/In-Context Search/4_ContactListSearch_Helper.js
@@ -0,0 +1,18 @@
+({
+
+    fetchContacts : function(component) {    
+        var action = component.get("c.getContacts");
+        
+        action.setParams({
+            pAccountId: component.get("v.recordId")
+        });
+        
+        action.setCallback(this, function(data) {
+            //console.log("data.getReturnValue --> " + data.getReturnValue());
+            component.set("v.Contacts", data.getReturnValue());
+        });
+        
+        $A.enqueueAction(action);	
+    }
+
+})
